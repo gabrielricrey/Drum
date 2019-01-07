65227307817b22e49dba503e9503436e9239d7f8
Gabriel Richter Reynoso <gabrielrr@gabriels-macbook-pro.local>
2019-01-07 10:53:48 +0100

    Initial Commit

---
+++ Drum/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Drum
+//
+//  Created by Gabriel Richter Reynoso on 2019-01-07.
+//  Copyright © 2019 Gabriel Richter Reynoso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
